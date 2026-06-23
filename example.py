e6846a964ff417fa71de1148b24abce7da8aaa36
Gleidson Viana <gleidson.social@outlook.com>
2026-06-23 01:22:45 -0300

    adicionando o pylintrc

---
+++ example.py
@@ -0,0 +1,17 @@
+"""
+docstring do modulo
+"""
+
+def essa_e_minha_funcao():
+    """
+    docstring da função
+    """
+    print("oi")
+
+def minha_funcao():
+    """
+    docstring da função
+    """
+    print("minha funcao")
+
+essa_e_minha_funcao()
